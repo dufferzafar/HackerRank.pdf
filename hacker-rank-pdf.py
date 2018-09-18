c3a1852de1c2520f2790b790225a870b90a3232e
Shadab Zafar <dufferzafar0@gmail.com>
2018-09-18 10:31:31 +0530

    Take category from CLI and output to a file

--- hacker-rank-pdf.py
+++ hacker-rank-pdf.py
@@ -1,18 +1,30 @@
-""" HackerRank PDF Downloader. """
+"""
+HackerRank PDF Downloader.
 
+Usage: hacker-rank-pdf.py <category> > category.sh
+"""
+
+import os
+import sys
 import time
 
 import requests
-
-CATEGORY = "dynamic-programming"
 
 ROOT = "https://www.hackerrank.com/rest/contests/master"
 CHALLENGES = ROOT + "/tracks/algorithms/challenges"
 PDF_URL = ROOT + "/challenges/{slug}/download_pdf?language=English"
 
-OUT_FMT = "wget -O '{file_name}' {url}"
+OUT_FMT = "wget -O \"{file_name}\" {url}"
 
-params = {"offset": 0, "limit": 10, "filters[subdomains][]": CATEGORY}
+category = sys.argv[1]
+params = {"offset": 0, "limit": 10, "filters[subdomains][]": category}
+
+dirname = " ".join(category.split("-")).title()
+if not os.path.isdir(dirname):
+    os.mkdir(dirname)
+
+fname = dirname + "/" + category + ".sh"
+output = open(fname, "w")
 
 # Wouldn't have to use this hack, if do-while loops exist
 questions = 1
@@ -22,14 +34,13 @@ while questions:
 
     for Q in questions:
 
-        file_name = Q["difficulty_name"] + " | " + Q["name"] + ".pdf"
+        file_name = Q["difficulty_name"] + " | " + Q["name"].strip() + ".pdf"
         url = PDF_URL.format(slug=Q['slug'])
 
-        print(OUT_FMT.format(file_name=file_name, url=url))
+        output.write(OUT_FMT.format(file_name=file_name, url=url))
+        output.write("\n")
 
     # Wait for some time between calls
     time.sleep(1)
 
     params['offset'] += params['limit']
-
-    break
